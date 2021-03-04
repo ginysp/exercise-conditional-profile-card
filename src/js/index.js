eb7f5d6daaad2f81d186fec5b54fd73f8c5a8c53
Gineth <ginysp@hotmail.com>
2021-03-04 01:41:04 +0000

    code changes

--- src/js/index.js
+++ src/js/index.js
@@ -27,20 +27,24 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-    if (variables.includeCover == false) cover = "<div class='cover'></div>";
-    if (variables.name === null) variables.name = "Name";
-    if (variables.lastname === null) variables.lastname = "Last Name";
-    if (variables.role === null) variables.role = "Role";
-
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name === null) variables.name = "Name";
+  if (variables.lastname === null) variables.lastname = "Last Name";
+  if (variables.role === null) variables.role = "Role";
+  if (variables.city === null) variables.city = "City";
+  if (variables.country === null) variables.country = "Country";
+  if (variables.socialMediaPosition === null)
+    variables.socialMediaPosition = "left";
+  if (variables.twitter === null || variables.twitter === "") variables.twitter = "Twitter";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>"${variables.name} ${variables.lastname}"</h1>
           <h2>"${variables.role}"</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
+          <h3>"${variables.city}", "${variables.country}"</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/"${variables.twitter}""><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
